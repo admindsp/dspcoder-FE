4b2faeb8bcb87f8d9e5f0ed116bd332a90b62e5b
Debraj Dhar <debraj.dhar@wakefit.co>
2024-04-10 05:05:13 +0530

    test

--- apps/next/src/app/components/Navbar/Navbar.tsx
+++ apps/next/src/app/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const Navbar = (props: Props) => {
   return (
     <div className="bg-[#1d241d] py-2 text-white px-2 flex gap-10">
-      <Link href="/">DSPCoder</Link>
+      <Link href="/">DSPCoder BAGCHI HAGU</Link>
       <div className="flex justify-between w-full">
         <Link href="/problem">Problems</Link>
         <div>icons</div>
