6e3a39ed57ed18670a398d3b3bbf76551b3de80a
Debraj Dhar <debraj.dhar@wakefit.co>
2024-04-09 15:20:00 +0530

    configured prettier and server setup

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,23 +1,23 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import Navbar from "./components/navbar/Navbar";
-import CodeEditor from "./components/CodeEditor/CodeEditor";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Navbar from './components/navbar/Navbar';
+import CodeEditor from './components/CodeEditor/CodeEditor';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "DSPCoder",
-  description: "A platform for embedded systems engineers",
+  title: 'DSPCoder',
+  description: 'A platform for embedded systems engineers'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html lang="en">  
+    <html lang="en">
       <body className={inter.className}>
         <Navbar />
         <CodeEditor />
