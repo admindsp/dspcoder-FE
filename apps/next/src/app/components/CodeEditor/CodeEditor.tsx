0e9ae643ac20d6a5b6de3bc6d050d9f38442bae6
Debraj Dhar <debraj.dhar@wakefit.co>
2024-04-10 17:04:46 +0530

    Update: added dark theme on code editor, in progress language selector.

--- apps/next/src/app/components/CodeEditor/CodeEditor.tsx
+++ apps/next/src/app/components/CodeEditor/CodeEditor.tsx
@@ -1,16 +1,18 @@
 "use client";
 import React from "react";
 import Editor from "@monaco-editor/react";
+import ConfigSelectorMenu from "./components/ConfigSelectorMenu";
 
 type Props = {};
 
 const CodeEditor = (props: Props) => {
   return (
     <div className="w-full">
+      <ConfigSelectorMenu />
       <Editor
-        className="border border-gray-500"
-        height="50vh"
-        defaultLanguage="javascript"
+        className="border border-t-0 border-gray-700 h-[50vh]"
+        theme="vs-dark"
+        defaultLanguage="cpp"
         defaultValue="// some comment"
       />
     </div>
---
+++ apps/next/src/app/components/CodeEditor/components/ConfigSelectorMenu.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const ConfigSelectorMenu = (props: Props) => {
+  return (
+    <div className="bg-[#000000] text-white flex px-4 py-1">
+      <div className="language-selector-menu">
+        <p className="text-sm">Language </p>
+      </div>
+    </div>
+  );
+};
+
+export default ConfigSelectorMenu;
