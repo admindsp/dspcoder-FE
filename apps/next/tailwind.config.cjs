613ae7f35ff3624e5c16362cee99f7a29f57ecbb
Debraj Dhar <debraj.dhar@wakefit.co>
2024-04-16 00:45:40 +0530

    added tailwind

--- apps/next/tailwind.config.cjs
+++ apps/next/tailwind.config.cjs
@@ -1,23 +1,3 @@
-// const baseConfig = require("@dspcoder/tailwind-config");
-// const tailwindAnimate = require("tailwindcss-animate");
-
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./src/**/*.{ts,tsx}",
-//     "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-//     "../../packages/ui/src/**/*.{js,ts,jsx,tsx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx}",
-//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-//     "*",
-//   ],
-//   presets: [baseConfig],
-//   plugins: [tailwindAnimate],
-// };
-
 const baseConfig = require("@dspcoder/tailwind-config/tailwind.config.js");
 
 module.exports = {
