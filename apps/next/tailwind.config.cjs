73cb506ca410f2de2a10a026bf7dba4a33b51bdf
Avijit Ghosh <gavijit622@gmail.com>
2024-04-17 00:29:22 +0530

    setup init

--- apps/next/tailwind.config.cjs
+++ apps/next/tailwind.config.cjs
@@ -1,17 +1,5 @@
-const baseConfig = require("@dspcoder/tailwind-config/tailwind.config.js");
+const baseConfig = require("@dspcoder/ui/tailwind.config");
 
 module.exports = {
-  content: [
-    "./src/**/*.{ts,tsx}",
-    "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    "../../packages/ui/src/**/*.{js,ts,jsx,tsx}",
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "*",
-  ],
   presets: [baseConfig],
-  plugins: [],
 };
