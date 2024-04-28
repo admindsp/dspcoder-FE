fe5ceeb1484ab0a0a7f2b7d0b5116cdfe3e1862e
Debraj550 <debrajdhar100@gmail.com>
2024-04-28 23:09:00 +0530

    Update: node mongo setup

---
+++ server/problem_management_service/app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 8001;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.json());
+
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json({ message: "CHOLCHE" });
+  } catch (error) {
+    res.status(500).json({ message: "GG" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
