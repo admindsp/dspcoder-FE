8ccd101c900725f6a6f3a12e760352e22acd54bc
Debraj550 <debrajdhar100@gmail.com>
2024-08-01 00:43:02 +0530

    update

---
+++ apps/next/src/app/problems/[problemId]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useRouter } from "next/navigation";
+
+type Props = {};
+
+const page = (props: Props) => {
+  const router = useRouter();
+  return <div>{router.query.problemId}</div>;
+};
+
+export default page;
