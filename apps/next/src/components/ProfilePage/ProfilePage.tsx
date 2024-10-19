71b635842ea148a88297e50de70c7608f354c043
Debraj550 <debrajdhar100@gmail.com>
2024-10-20 01:51:24 +0530

    update

--- apps/next/src/components/ProfilePage/ProfilePage.tsx
+++ apps/next/src/components/ProfilePage/ProfilePage.tsx
@@ -8,6 +8,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@dspcoder/ui/components/ui/dropdown-menu";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@dspcoder/ui/components/ui/avatar";
 import { signOut, useSession } from "next-auth/react";
 import React from "react";
 import { IoIosLogOut } from "react-icons/io";
@@ -24,17 +29,17 @@ const ProfilePage = (props: Props) => {
     <div className="">
       <DropdownMenu>
         <DropdownMenuTrigger className="flex gap-2 items-center">
-          <img
-            src={`${session?.user?.image}.png`}
-            alt="profile-picture"
-            width={25}
-            height={25}
-            className="rounded-full"
-          />
+          <Avatar className="max-w-[30px] max-h-[30px] border border-whitish">
+            <AvatarImage
+              alt="profile-picture"
+              src={`${session?.user?.image}.png`}
+            />
+            <AvatarFallback>{session?.user?.name}</AvatarFallback>
+          </Avatar>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="">
+        <DropdownMenuContent className="min-w-[200px] p-0">
           <DropdownMenuLabel>
-            Hi, <span className="font-semibold">{session?.user?.name}</span>
+            Hi, <span className="font-bold">{session?.user?.name}</span>
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem>
