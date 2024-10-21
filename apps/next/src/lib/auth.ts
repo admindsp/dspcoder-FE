d247069cace4a63ad5923c131366c5332a5afe30
admindsp <admin@dspcoder.com>
2024-10-21 12:20:34 +0530

    Update auth.ts

--- apps/next/src/lib/auth.ts
+++ apps/next/src/lib/auth.ts
@@ -7,7 +7,6 @@ export const authConfig: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID as string,
       clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET as string,
-      authorization: { url: "https://github" },
     }),
   ],
   secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
