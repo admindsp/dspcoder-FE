2b8a7e77f47b1b3023e680f9a42ffb9e6d2a4df8
Debraj550 <debrajdhar100@gmail.com>
2025-03-23 10:27:49 +0530

    updated package

--- packages/ui/src/components/ui/index.ts
+++ packages/ui/src/components/ui/index.ts
@@ -1,2 +1,7 @@
 export * from "./use-toast";
 export * from "./use-mobile";
+export * from "./card";
+export * from "./accordion";
+export * from "./avatar";
+export * from "./badge";
+export * from "./tabs";
